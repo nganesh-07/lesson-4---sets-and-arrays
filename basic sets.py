7c5615b5bd359d9d1c4c9e092041266da4fba339
nganesh <nganesh@iu.edu>
2025-06-24 19:24:29 -0700

    basic sets

---
+++ basic sets.py
@@ -0,0 +1,16 @@
+set1 = {1, 2, 3}
+print(set1)
+
+set2 = {2.5, "Hello", (1, 5, 7)}
+print(set2)
+
+set3 = {1, 2, 3, 5, 2, 3}
+print(set3)
+
+set4 = set([1, 5, 4, 3])
+print(set4)
+
+numberset = set([0, 1, 2, 3, 4, 5, 6, 7])
+print("Original set of numbers:", numberset)
+numberset.pop()
+print("After removing the first element from this list, the final list is:", numberset)
