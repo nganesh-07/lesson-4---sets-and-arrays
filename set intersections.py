68c33ed4ce39773b8eea0d57bca7e6a8623be311
nganesh <nganesh@iu.edu>
2025-06-24 19:27:38 -0700

    set intersection

---
+++ set intersections.py
@@ -0,0 +1,12 @@
+set1 = {"purple", "pink"}
+set2 = {"pink", "white"}
+
+print("Our two sets:")
+print(set1)
+print(set2)
+
+print("The commonality between these two sets:")
+
+set3 = set1.intersection(set2)
+
+print(set3)
